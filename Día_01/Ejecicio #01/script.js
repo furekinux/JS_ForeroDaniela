6ce7075bd71b3ce2e09cadeecbd71af8c913223b
Daniela Forero Ballén <156367697+furekinux@users.noreply.github.com>
2024-04-11 08:55:00 -0500

    chore: :truck: Subir de ejercicio #01

--- Día_01/Ejecicio #01/script.js
+++ Día_01/Ejecicio #01/script.js
@@ -3,3 +3,13 @@
 
 //              DANIELA FORERO BALLÉN (P1)
 //#######################################################
+
+console.log("Ingrese el valor de grados celcius para convertirlo a Farenheit")
+
+var celcius=20
+
+celcius=prompt("Qué valor desea convertir?")
+
+var farenheit= 32+(9*celcius/5)
+
+console.log(farenheit)
