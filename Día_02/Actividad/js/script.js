e22d2518ff1e2e8412f73025609b162a6a955668
Daniela Forero Ballén <156367697+furekinux@users.noreply.github.com>
2024-04-12 14:09:32 -0500

    feat: :hammer: Se modificó el archivo JS
    Se realizó una actualización al archivo script.js para READ al CRUD, primer intento de CREATE para ingreso de camper nuevo

--- Día_02/Actividad/js/script.js
+++ Día_02/Actividad/js/script.js
@@ -1,33 +1,58 @@
 let userchoice = "0"
 
-let campers = {
+var campers = {
     "001":{
         id: "123",
-        name: "a",
-        secname: "dg fbg"
+        nombre: "a",
+        apellidos: "dg fbg",
+        sala:"Art",
+        ruta:"Java"
     },
     "002":{
         id: "456",
+        nombre: "e",
+        apellidos: "dvfd fdvf",
+        sala: "Sput",
+        ruta: "NoteDS"
+    }
+}
+
+var trainers = {
+    "pedro":{
+        id: "123",
+        name: "a",
+        secname: "dg fbg",
+        sala:"art"
+    },
+    "miguel":{
+        id: "456",
         name: "e",
-        secname: "dvfd fdvf"
+        secname: "dvfd fdvf",
+        sala: "sput"
     }
 }
 
 
 console.log("%c ------Menú Principal------\n   Bienvenido usuario, seleccione una opción:\n -> (1)Campers\n -> (2)Trainers\n -> (3)Registar nuevos campers\n -> (4)Salir", "color:green;");
-userchoice = prompt("-> (1)Campers\n -> (2)Trainers\n -> (3)Registar nuevos campers\n -> (4)Salir");
+userchoice = prompt(" -> (1)Campers\n -> (2)Trainers\n -> (3)Registar nuevos campers\n -> (4)Salir");
 
 if (userchoice == "1"){
-
+    console.clear()
     console.log("%c ------Menú Camper------\n   Seleccione la acción a realizar:\n -> (1)Ver Informacion\n -> (2)Ver Notas\n -> (3)Ver grupos", "color:blue;");
+    var rta = prompt("-> (1)Ver Informacion\n -> (2)Ver Notas\n -> (3)Ver grupos");
+    if (rta == "1"){
+        console.table(campers);
+    };
 
     
 } else {
 
     if (userchoice == "2"){
-
-        console.log("%c ------Menú Trainer------\n   Seleccione la acción a realizar:\n -> (1)Campers\n -> (2)Trainers\n -> (3)Registar nuevos campers\n -> (4)Salir", "color:red;");
-        
+        console.clear()
+        console.log("%c ------Menú Trainer------\n   Seleccione la acción a realizar:\n -> (1)Ver informacion\n -> (2)Asignar Sala\n -> (3)Registar nuevos campers\n -> (4)Salir", "color:red;");
+        if (rta == "1"){
+            console.table(trainers);
+        };
     } else {
 
         if (userchoice == "3"){
@@ -36,9 +61,26 @@ if (userchoice == "1"){
             campname = prompt("-> Nombre(s) del Camper");
             campsecname = prompt("-> Apellidos del Camper");
             campid = prompt("-> N° de identificación del camper");
+            Number=userid = prompt("-> ID de camper");
+
             console.log("%c |-----Nuevo camper-----|", "color:#ff0099;","\n   ID          ", campid,"\n   Nombre(s)   ", campname,"\n   Apellidos   ", campsecname);
-            rta = prompt("¿Confirmar estos datos?");
+            
+            let newcamp= {
+                "${userid}":{
+                id: campid,
+                nombre: campname,
+                apellidos: campsecname,
+                sala:"pendiente",
+                ruta:"pendiente",
+                }
+            }
+
+            console.table(newcamp);
+
+            var rta = prompt("¿Confirmar estos datos?");
             if (rta=="si"){
+                campers.push(newcamp);
+                console.table(campers);
 
             }
 
