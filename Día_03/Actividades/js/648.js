5bd306e2adecb84b18b708efa03fa75a5df1bff7
Daniela Forero Ballén <156367697+furekinux@users.noreply.github.com>
2024-04-16 14:52:26 -0500

    feat: :sparkles: Actividad Replace Words completada
    Se completó la actividad de replace words:
    https://leetcode.com/problems/replace-words/

--- Día_03/Actividades/js/648.js
+++ Día_03/Actividades/js/648.js
@@ -5,10 +5,31 @@
  */
 
 
-dictionary = ["cat","bat","rat"]
-sentence = "the cattle was rattled by the battery"
-
 var replaceWords = function(dictionary, sentence) {
-    newdict = sentence.split(" ")
-    console.log(newdict)
+    newdict = sentence.split(" "); //newdict = ["the","cat", "was", "rat","by","the","bat"]
+    for(animal in dictionary){ //bat
+
+        for(word in newdict){ //rattled
+
+            let compareme = newdict[word].slice(0,3);//"cat" tle
+
+            if(dictionary[animal]==compareme){//cat==cat tle
+                newdict.splice(word,1,dictionary[animal]);
+                
+            } else{
+                compareme = newdict[word].slice(0,1);
+                if(dictionary[animal]==compareme){
+                    newdict.splice(word,1,dictionary[animal]);
+                }
+            }
+        }
+
+    }
+    sumthedict=""
+//sumthedict = "the cat was rat by the bat"
+    sumthedict= newdict.join(' ');
+
+    return(sumthedict)
 };
+
+// \(:D)/
